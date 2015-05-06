a59ca2c9e70e33c85e870c74b99f1a649093ef0e
Kevin Chen & jocelyn alsdorf <student@epicodus.com>
2015-05-06 10:14:24 -0700

    Create CRUD for Actor and Movie class

---
+++ spec/actor_spec.rb
@@ -0,0 +1,62 @@
+require("spec_helper")
+
+describe(Actor) do
+
+  describe("#name") do
+    it("returns the name") do
+      actor = Actor.new({:name => "George Clooney", :id => nil})
+      expect(actor.name()).to(eq("George Clooney"))
+    end
+  end
+
+  describe("#id") do
+    it("returns the id") do
+      actor = Actor.new({:name => "George Clooney", :id => 1})
+      expect(actor.id()).to(eq(1))
+    end
+  end
+
+  describe(".all") do
+    it("starts off with no actors") do
+      expect(Actor.all()).to(eq([]))
+    end
+  end
+
+  describe(".find") do
+    it("returns an actor by its ID number") do
+      test_actor = Actor.new({:name => "George Clooney", :id => nil})
+      test_actor.save()
+      test_actor2 = Actor.new({:name => "Brad Pitt", :id => nil})
+      test_actor2.save()
+      expect(Actor.find(test_actor2.id())).to(eq(test_actor2))
+    end
+  end
+
+  describe("#==") do
+    it("is the same actor if it has the same name and id") do
+      actor = Actor.new({:name => "George Clooney", :id => nil})
+      actor2 = Actor.new({:name => "George Clooney", :id => nil})
+      expect(actor).to(eq(actor2))
+    end
+  end
+
+  describe("#update") do
+    it("lets you update actors in the database") do
+      actor = Actor.new({:name => "George Clooney", :id => nil})
+      actor.save()
+      actor.update({:name => "Brad Pitt"})
+      expect(actor.name()).to(eq("Brad Pitt"))
+    end
+  end
+
+  describe("#delete") do
+    it("lets you delete a actor from the database") do
+      actor = Actor.new({:name => "George Clooney", :id => nil})
+      actor.save()
+      actor2 = Actor.new({:name => "Brad Pitt", :id => nil})
+      actor2.save()
+      actor.delete()
+      expect(Actor.all()).to(eq([actor2]))
+    end
+  end
+end
