6cc6b7052e8b62da3148de982c06304589f9a151
Kevin Chen & jocelyn alsdorf <student@epicodus.com>
2015-05-06 09:41:50 -0700

    Create spec helper file

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require('rspec')
+require('pg')
+require('actor')
+require('movie')
+
+DB = PG.connect({:dbname => 'movie_database_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM movies *;")
+    DB.exec("DELETE FROM actors *;")
+    DB.exec("DELETE FROM movies_actors *;")
+  end
+end
